b729090666e4b15ea0e7b0e079a69014ea00eb4a
Adrian Kennard <a@k.gg>
2021-05-16 13:37:04 +0100

    Library update

--- main/BatMon.c
+++ main/BatMon.c
@@ -11,7 +11,6 @@ uint8_t busy = 0;               // Don't sleep
 
 const char *app_command(const char *tag, unsigned int len, const unsigned char *value)
 {
-	ESP_LOGE(TAG,"%s",tag);
    if (!strcmp(tag, "upgrade"))
       busy = 1;
    return "";
@@ -21,22 +20,22 @@ void app_main()
 {
    ESP_LOGE(TAG, "Start");
    revk_init(&app_command);
+   for (int t = 100; t; t--)
+      if (revk_offline())
+         usleep(100000);
+      else
+         break;
    // Do some stuff...
+   // Now to sleep
    sleep(10);
-   while (1)
-   {
-      if (busy)
-      {
-         sleep(1);
-         continue;
-      }
-      time_t now = time(0);
-      ulp_time = 1000 * (60 - (now % 60));;
-      ESP_LOGE(TAG, "Going to sleep %dms", ulp_time);
-      ulp_init();
-      ulp_start();
-      esp_sleep_enable_ulp_wakeup();
-      esp_deep_sleep_start();
-      ESP_LOGE(TAG, "awake");
-   }
+   if (busy)
+      return;
+   time_t now = time(0);
+   ulp_time = 1000 * (60 - (now % 60));;
+   ESP_LOGE(TAG, "Going to sleep %dms", ulp_time);
+   ulp_init();
+   ulp_start();
+   esp_sleep_enable_ulp_wakeup();
+   esp_deep_sleep_start();
+   ESP_LOGE(TAG, "awake");
 }
