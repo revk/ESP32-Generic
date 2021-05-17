2b7f251d63a8edf07198c509cf21ad1b61f847bc
Adrian Kennard <a@k.gg>
2021-05-17 09:57:52 +0100

    Tidy

--- main/BatMon.c
+++ main/BatMon.c
@@ -7,10 +7,24 @@ static const char TAG[] = "BatMon";
 #include "esp32/ulp.h"
 #include "ulp-main.h"
 
+#define	settings		\
+	u32(period,60)	\
+	u32(awake,10)	\
+
+#define u32(n,d)        uint32_t n;
+#define s8(n,d) int8_t n;
+#define u8(n,d) uint8_t n;
+#define b(n) uint8_t n;
+#define s(n) char * n;
+settings
+#undef u32
+#undef s8
+#undef u8
+#undef b
+#undef s
 #ifndef CONFIG_ESP32_ULP_COPROC_ENABLED
 #error Set CONFIG_ESP32_ULP_COPROC_ENABLED
 #endif
-
 extern const uint8_t _ulp_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t _ulp_end[] asm("_binary_ulp_main_bin_end");
 
@@ -36,9 +50,20 @@ const char *app_command(const char *tag, unsigned int len, const unsigned char *
 
 void app_main()
 {
-   ESP_LOGE(TAG, "Start %ld", time(0) % 60);
+   ESP_LOGE(TAG, "Start %ld", time(0) % period);
    revk_init(&app_command);
-   for (int t = 100; t; t--)
+#define b(n) revk_register(#n,0,sizeof(n),&n,NULL,SETTING_BOOLEAN);
+#define u32(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
+#define s8(n,d) revk_register(#n,0,sizeof(n),&n,#d,SETTING_SIGNED);
+#define u8(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
+#define s(n) revk_register(#n,0,0,&n,NULL,0);
+   settings
+#undef u32
+#undef s8
+#undef u8
+#undef b
+#undef s
+       for (int t = 100; t; t--)
       if (revk_offline())
          usleep(100000);
       else
@@ -48,7 +73,7 @@ void app_main()
       sleep(1);                 // Wait clock set
    // Do some stuff...
    // Now to sleep
-   sleep(1);
+   sleep(awake);
    if (busy)
    {
       ESP_LOGE(TAG, "Waiting");
@@ -61,8 +86,10 @@ void app_main()
    struct timeval tv;
    struct timezone tz;
    gettimeofday(&tv, &tz);
-   ulp_time = 1000 * (59 - (tv.tv_sec % 60)) + (1000 - tv.tv_usec / 1000);
-   ESP_LOGE(TAG, "Going to sleep %dms", ulp_time );
+   if (!period)
+      period = 60; // Avoid a divide by zero
+   ulp_time = 1000 * ((period - 1) - (tv.tv_sec % period)) + (1000 - tv.tv_usec / 1000);
+   ESP_LOGE(TAG, "Going to sleep %dms", ulp_time);
    esp_sleep_enable_ulp_wakeup();
    ulp_start();
    ESP_LOGE(TAG, "Deep sleep");
