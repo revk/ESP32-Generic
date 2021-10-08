dbabbc1ba5ba36e09a682d0d3d96e90a455de258
Adrian Kennard <a@k.gg>
2021-10-08 14:05:23 +0100

    Working on cmake now

--- main/Generic.c
+++ main/Generic.c
@@ -27,10 +27,10 @@ static const char TAG[] = "Generic";
 #endif
 
 #define	MAXGPIO	36
-static uint8_t  input[MAXGPIO];
-static uint8_t  output[MAXGPIO];
-static uint8_t  power[MAXGPIO]; /* fixed outputs */
-int             holding = 0;
+static uint8_t input[MAXGPIO];
+static uint8_t output[MAXGPIO];
+static uint8_t power[MAXGPIO];  /* fixed outputs */
+int holding = 0;
 
 #define	settings		\
 	u32(period,3600)	\
@@ -62,22 +62,20 @@ settings
 #undef u8
 #undef b
 #undef s
-uint64_t busy = 0;
-char            usb_present = 0;
-char            charger_present = 0;
-const char     *rangererr = NULL;
-uint16_t        range = 0;
-uint32_t        voltage = 0;
+    uint64_t busy = 0;
+char usb_present = 0;
+char charger_present = 0;
+const char *rangererr = NULL;
+uint16_t range = 0;
+uint32_t voltage = 0;
 
-const char     *
-app_callback(int client, const char *prefix, const char *target, const char *suffix, jo_t j)
+const char *app_callback(int client, const char *prefix, const char *target, const char *suffix, jo_t j)
 {
    if (client || !prefix || target || strcmp(prefix, prefixcommand) || !suffix)
       return NULL;
-   //Not for us
-      or not a command from main MQTT
-         char            value[100];
-   int             len = 0;
+   // Not for us or not a command from main MQTT
+   char value[100];
+   int len = 0;
    if (j)
    {
       len = jo_strncpy(j, value, sizeof(value));
@@ -98,15 +96,15 @@ app_callback(int client, const char *prefix, const char *target, const char *suf
    }
    if (!strncmp(suffix, "output", 6))
    {
-      int             i = atoi(suffix + 6);
+      int i = atoi(suffix + 6);
       if (i < 0 || i >= MAXGPIO)
          return "Bad output number";
       if (!output[i])
          return "Output not configured";
-      int             v = 0;
+      int v = 0;
       if (len && (*value == '1' || *value == 't' || *value == 'T' || *value == 'y' || *value == 'Y'))
          v = 1;
-      int             p = output[i] & 0x3F;
+      int p = output[i] & 0x3F;
       REVK_ERR_CHECK(gpio_hold_dis(p));
       REVK_ERR_CHECK(gpio_set_level(p, ((output[i] & 0x40) ? 1 : 0) ^ v));
       REVK_ERR_CHECK(gpio_hold_en(p));
@@ -115,10 +113,9 @@ app_callback(int client, const char *prefix, const char *target, const char *suf
    return NULL;
 }
 
-void
-app_main()
+void app_main()
 {
-   time_t          now = time(0);
+   time_t now = time(0);
    revk_boot(&app_callback);
 #define io(n,d)           revk_register(#n,0,sizeof(n),&n,"- "#d,SETTING_SET|SETTING_BITFIELD);
 #define b(n) revk_register(#n,0,sizeof(n),&n,NULL,SETTING_BOOLEAN);
@@ -133,23 +130,23 @@ app_main()
 #undef u8
 #undef b
 #undef s
-      revk_register("input", MAXGPIO, sizeof(*input), &input, "-", SETTING_BITFIELD | SETTING_SET);
+       revk_register("input", MAXGPIO, sizeof(*input), &input, "-", SETTING_BITFIELD | SETTING_SET);
    revk_register("output", MAXGPIO, sizeof(*output), &output, "-", SETTING_BITFIELD | SETTING_SET);
    revk_register("power", MAXGPIO, sizeof(*power), &power, "-", SETTING_BITFIELD | SETTING_SET);
    revk_start();
-   int             i;
+   int i;
    for (i = 0; i < MAXGPIO; i++)
    {
       if (input[i])
       {
-         int             p = input[i] & 0x3F;
+         int p = input[i] & 0x3F;
          REVK_ERR_CHECK(gpio_hold_dis(p));
          REVK_ERR_CHECK(gpio_reset_pin(p));
          REVK_ERR_CHECK(gpio_set_direction(p, GPIO_MODE_INPUT));
       }
       if (output[i])
       {
-         int             p = output[i] & 0x3F;
+         int p = output[i] & 0x3F;
          REVK_ERR_CHECK(gpio_reset_pin(p));
          REVK_ERR_CHECK(gpio_set_level(p, (output[i] & 0x40) ? 1 : 0)); /* Off */
          REVK_ERR_CHECK(gpio_set_direction(p, GPIO_MODE_OUTPUT));
@@ -158,7 +155,7 @@ app_main()
       }
       if (power[i])
       {
-         int             p = power[i] & 0x3F;
+         int p = power[i] & 0x3F;
          REVK_ERR_CHECK(gpio_reset_pin(p));
          REVK_ERR_CHECK(gpio_set_level(p, (power[i] & 0x40) ? 0 : 1));
          REVK_ERR_CHECK(gpio_set_drive_capability(p, GPIO_DRIVE_CAP_3));
@@ -172,7 +169,7 @@ app_main()
    if (esp_reset_reason() != ESP_RST_DEEPSLEEP && awake < 60)
       awake = 60;
    //Power up
-      ESP_LOGI(TAG, "Start %ld", now % period);
+   ESP_LOGI(TAG, "Start %ld", now % period);
    if (usb)
    {
       gpio_reset_pin(usb & 0x3F);
@@ -217,7 +214,7 @@ app_main()
       REVK_ERR_CHECK(gpio_set_level(adcon & 0x3F, (adcon & 0x40) ? 0 : 1));     /* on */
       REVK_ERR_CHECK(gpio_set_direction(adcon & 0x3F, GPIO_MODE_OUTPUT));
 
-      esp_adc_cal_characteristics_t adc_chars = {0};
+      esp_adc_cal_characteristics_t adc_chars = { 0 };
       esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
       REVK_ERR_CHECK(adc1_config_width(ADC_WIDTH_BIT_12));
       REVK_ERR_CHECK(adc1_config_channel_atten(adc, ADC_ATTEN_DB_11));
@@ -225,7 +222,7 @@ app_main()
       voltage = 0;
       for (int try = 0; try < TRIES; try++)
       {
-         uint32_t        v = 0;
+         uint32_t v = 0;
          REVK_ERR_CHECK(esp_adc_cal_get_voltage(adc, &adc_chars, &v));
          if (adcr2)
             v = v * (adcr1 + adcr2) / adcr2;
@@ -258,7 +255,7 @@ app_main()
       ESP_LOGI(TAG, "Ranger init GND=%d PWR=%d SCL=%d SDA=%d Address=%02X", rangergnd & 0x3F, rangerpwr & 0x3F, rangerscl & 0x3F, rangersda & 0x3F, rangeraddress);
       if (ranger0x)
       {
-         vl53l0x_t      *v = vl53l0x_config(0, rangerscl & 0x3F, rangersda & 0x3F, -1, rangeraddress, 0);
+         vl53l0x_t *v = vl53l0x_config(0, rangerscl & 0x3F, rangersda & 0x3F, -1, rangeraddress, 0);
          if (!v)
             ESP_LOGE(TAG, "Ranger config failed");
          else
@@ -274,7 +271,7 @@ app_main()
          }
       } else
       {                         /* Try 1X */
-         vl53l1x_t      *v = vl53l1x_config(0, rangerscl & 0x3F, rangersda & 0x3F, -1, rangeraddress, 0);
+         vl53l1x_t *v = vl53l1x_config(0, rangerscl & 0x3F, rangersda & 0x3F, -1, rangeraddress, 0);
          if (!v)
             ESP_LOGE(TAG, "Ranger config failed");
          else
@@ -305,16 +302,16 @@ app_main()
          while ((now = time(0)) < 30)
             sleep(1);
       }
-      int64_t         run = esp_timer_get_time();
-      struct tm       tm;
+      int64_t run = esp_timer_get_time();
+      struct tm tm;
       gmtime_r(&now, &tm);
       if (!tm.tm_hour && !tm.tm_min && awake < 10)
          awake = 10;            /* allow clock to set */
-      jo_t            j = jo_create_alloc();
+      jo_t j = jo_create_alloc();
       jo_object(j, NULL);
       jo_string(j, "id", revk_id);
       jo_stringf(j, "ts", "%04d-%02d-%02dT%02d:%02d:%02dZ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-      jo_litf(j, "runtime", "%u.%06u", (int)run / 1000000, (int)run % 1000000);
+      jo_litf(j, "runtime", "%u.%06u", (int) run / 1000000, (int) run % 1000000);
       if (range)
          jo_litf(j, "range", "%d", range);
       if (voltage)
@@ -344,7 +341,7 @@ app_main()
    }
    if (busy)
    {
-      ESP_LOGI(TAG, "Waiting %d", (int)((busy - esp_timer_get_time()) / 1000000ULL));
+      ESP_LOGI(TAG, "Waiting %d", (int) ((busy - esp_timer_get_time()) / 1000000ULL));
       while (busy > esp_timer_get_time())
       {
          if (led)
@@ -357,10 +354,10 @@ app_main()
    }
    if (led)
       gpio_set_level(led & 0x3F, (led & 0x40) ? 1 : 0); /* Off */
-   time_t          next = (time(0) + 5) / period * period + period;
+   time_t next = (time(0) + 5) / period * period + period;
    {
-      char            reason[100];
-      struct tm       tm;
+      char reason[100];
+      struct tm tm;
       gmtime_r(&next, &tm);
       sprintf(reason, "Sleep after %lldms until %04d-%02d-%02dT%02d:%02d:%02dZ", esp_timer_get_time() / 1000, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
       revk_mqtt_close(reason);
@@ -374,7 +371,7 @@ app_main()
    }
    if (usb_present && !charger_present)
       sleep(1);
-   struct timeval  tv;
+   struct timeval tv;
    gettimeofday(&tv, NULL);
    if (next < tv.tv_sec + 1)
       next = tv.tv_sec + 1;
